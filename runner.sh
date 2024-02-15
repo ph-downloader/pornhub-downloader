e51ce92d37e3d8e2015293f1a8de4e4e3e16141f
Andy Chen <ph-downloader@hotmail.com>
2024-02-15 11:01:39 -0800

    Update permissions

--- runner.sh
+++ runner.sh
@@ -4,5 +4,7 @@ while true
 do
     echo "Current run: `date`"
     poetry run python pornhub_downloader/downloader.py
+    echo "Changing ownership of the files"
+    chown -R "${PUID}:${PGID}" downloads
     sleep 3600
 done
